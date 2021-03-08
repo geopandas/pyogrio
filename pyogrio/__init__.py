05e105988d3f2fcd17c12f878623ee68f913b5e7
Brendan C. Ward <bcward@astutespruce.com>
2021-03-08 14:05:18 -0800

    Adapt fiona windows CI config

--- pyogrio/__init__.py
+++ pyogrio/__init__.py
@@ -1,2 +1,11 @@
+from pathlib import Path
+import os
+import sys
+
+if sys.platform == "win32":
+    # GDAL DLLs are copied to ".libs" folder; make sure this is on the PATH
+    libdir = Path(__file__) / ".libs"
+    os.environ["PATH"] = os.environ["PATH"] + ";" + libdir
+
 from pyogrio.core import list_layers, read_info
 from pyogrio.geopandas import read_dataframe, write_dataframe
