33d42e7d950502d7b607225ec415a4be6f091ca9
Brendan C. Ward <bcward@astutespruce.com>
2021-03-09 20:51:52 -0800

    Try forward slashes and log path

--- pyogrio/__init__.py
+++ pyogrio/__init__.py
@@ -1,11 +1,7 @@
-from pathlib import Path
 import os
-import sys
 
-if sys.platform == "win32":
-    # GDAL DLLs are copied to ".libs" folder; make sure this is on the PATH
-    libdir = str(Path(__file__) / ".libs")
-    os.environ["PATH"] = os.environ["PATH"] + ";" + libdir
+
+print("Incoming PATH: ", os.environ['PATH'])
 
 from pyogrio.core import list_layers, read_info
 from pyogrio.geopandas import read_dataframe, write_dataframe
