6b1d1a09e53aa4d6bbe960f93dfb60c07a0426b2
Brendan C. Ward <bcward@astutespruce.com>
2021-03-13 06:59:32 -0800

    Add more logging of env and try to use system calls to load GDAL to uncover error

--- pyogrio/__init__.py
+++ pyogrio/__init__.py
@@ -5,6 +5,9 @@ if sys.platform == "win32":
     import imp
     import os
 
+    print("Incoming environment")
+    print(os.environ)
+
     print("Trying to load GDAL dll directly")
     try:
         ctypes.WinDLL("gdal302.dll")
@@ -21,6 +24,21 @@ if sys.platform == "win32":
     except Exception as e:
         print("ERROR: still unable to load GDAL DLL")
         print(e)
+
+        # Try to load GDAL using system calls
+        kernel32 = ctypes.WinDLL("kernel32.dll", use_last_error=True)
+        res = kernel32.LoadLibraryExW("c:\\gdal\\bin\\gdal302.dll", 0, 0x00001100)
+        last_error = ctypes.get_last_error()
+        err = ctypes.WinError(last_error)
+        print("Error from loading GDAL")
+        print(err)
+
+        res = kernel32.LoadLibraryW("c:\\gdal\\bin\\gdal302.dll")
+        last_error = ctypes.get_last_error()
+        err = ctypes.WinError(last_error)
+        print("Error from loading GDAL(2)")
+        print(err)
+
 
 from pyogrio.core import list_layers, read_info
 from pyogrio.geopandas import read_dataframe, write_dataframe
