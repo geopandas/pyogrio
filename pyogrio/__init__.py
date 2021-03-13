88a3d24f27940730c82ce95a13b5969b257888fa
Brendan C. Ward <bcward@astutespruce.com>
2021-03-12 20:37:54 -0800

    Try again with more logging of GDAL DLL in test

--- pyogrio/__init__.py
+++ pyogrio/__init__.py
@@ -1,3 +1,27 @@
+import sys
+
+if sys.platform == "win32":
+    import ctypes
+    import imp
+    import os
+
+    print("Trying to load GDAL dll directly")
+    try:
+        ctypes.WinDLL("gdal302.dll")
+        print("successfully loaded GDAL DLL")
+    except Exception as e:
+        print("ERROR: unable to load GDAL DLL")
+        print(e)
+
+    print("Trying to load GDAL dll directly after setting GDAL path")
+    os.add_dll_directory("c:/gdal/bin")
+    try:
+        ctypes.WinDLL("gdal302.dll")
+        print("Successfully loaded GDAL DLL on second try")
+    except Exception as e:
+        print("ERROR: still unable to load GDAL DLL")
+        print(e)
+
 from pyogrio.core import list_layers, read_info
 from pyogrio.geopandas import read_dataframe, write_dataframe
 from pyogrio._version import get_versions
