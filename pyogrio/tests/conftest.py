769553b42ce2401ded78a21316fe6a0a5ab94489
theroggy <pieter.roggemans@gmail.com>
2022-04-27 09:50:37 +0200

    Add some extensions to ALL_EXTS
    To improve test coverage

--- pyogrio/tests/conftest.py
+++ pyogrio/tests/conftest.py
@@ -8,7 +8,7 @@ import pyogrio
 
 
 _data_dir = Path(__file__).parent.resolve() / "fixtures"
-ALL_EXTS = [".shp", ".gpkg", ".json"]
+ALL_EXTS = [".shp", ".gpkg", ".geojson", ".geojsons", ".fgb"]
 
 
 def pytest_report_header(config):
