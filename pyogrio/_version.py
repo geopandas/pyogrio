63693b5809d5dcf7bf4d7553a67926ae47a678cf
Brendan C. Ward <bcward@astutespruce.com>
2023-01-19 14:01:24 -0800

    Set parentdir_prefix

--- pyogrio/_version.py
+++ pyogrio/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg.VCS = "git"
     cfg.style = "pep440"
     cfg.tag_prefix = "v"
-    cfg.parentdir_prefix = "None"
+    cfg.parentdir_prefix = "pyogrio-"
     cfg.versionfile_source = "pyogrio/_version.py"
     cfg.verbose = False
     return cfg
