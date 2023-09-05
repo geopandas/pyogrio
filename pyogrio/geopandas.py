c1b27ad0dc6d3369ab9262cf932932ef406a9cec
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-09-05 22:04:16 +0200

    Update pyogrio/geopandas.py
    Co-authored-by: Brendan Ward <bcward@astutespruce.com>

--- pyogrio/geopandas.py
+++ pyogrio/geopandas.py
@@ -106,7 +106,7 @@ def read_dataframe(
         The sql dialect the sql statement is written in. Possible values:
 
           - **None**: if the datasource natively supports SQL, its specific SQL dialect
-            should be used (eg. SQLite and Geopackage: `SQLITE`_, PostgreSQL). If the
+            will be used by default (eg. SQLite and Geopackage: `SQLITE`_, PostgreSQL). If the
             datasource doesn't natively support SQL, the `OGRSQL`_ dialect is the
             default.
           - '`OGRSQL`_': can be used on any datasource. Performance can suffer
