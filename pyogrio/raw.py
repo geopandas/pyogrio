2d5deb727b97dfd41930abe84bb86047262414f6
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-09-05 22:03:33 +0200

    Update pyogrio/raw.py
    Co-authored-by: Brendan Ward <bcward@astutespruce.com>

--- pyogrio/raw.py
+++ pyogrio/raw.py
@@ -120,7 +120,7 @@ def read(
     sql_dialect : str, optional (default: None)
         The SQL dialect the ``sql`` statement is written in. Possible values:
 
-          - **None**: if the datasource natively supports sql, its specific sql dialect
+          - **None**: if the datasource natively supports SQL, its specific SQL dialect
             should be used (eg. SQLite and Geopackage: `SQLITE`_, PostgreSQL). If the
             datasource doesn't natively support sql, the `OGRSQL`_ dialect is the
             default.
