a28ac9cbbd7afdd3a3ea275149e4ea1a143ac864
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-12-03 02:12:17 +0100

    Improve doc

--- pyogrio/geopandas.py
+++ pyogrio/geopandas.py
@@ -155,7 +155,8 @@ def read_dataframe(
         is also driver specific and dependent on the value of ``use_arrow``. The order
         of the rows returned is undefined. If you would like to sort based on FID, use
         ``fid_as_index=True`` to have the index of the GeoDataFrame returned set to the
-        FIDs of the features read.
+        FIDs of the features read. If ``use_arrow=True``, the number of FIDs is limited
+        to 4997 for drivers with 'OGRSQL' as default SQL dialect.
     sql : str, optional (default: None)
         The SQL statement to execute. Look at the sql_dialect parameter for more
         information on the syntax to use for the query. When combined with other
@@ -340,7 +341,7 @@ def write_dataframe(
         in the output file.
     path : str
         path to file
-    layer :str, optional (default: None)
+    layer : str, optional (default: None)
         layer name
     driver : string, optional (default: None)
         The OGR format driver used to write the vector file. By default write_dataframe
@@ -540,7 +541,7 @@ def write_dataframe(
         # if possible use EPSG codes instead
         epsg = geometry.crs.to_epsg()
         if epsg:
-            crs = f"EPSG:{epsg}"
+            crs = f"EPSG:{epsg}"  # noqa: E231
         else:
             crs = geometry.crs.to_wkt(WktVersion.WKT1_GDAL)
 
