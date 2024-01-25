145ff00853c711613adbdb98e65fb3e960900206
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-01-25 09:46:31 +0100

    Update pyogrio/_compat.py
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>

--- pyogrio/_compat.py
+++ pyogrio/_compat.py
@@ -30,7 +30,7 @@ HAS_GEOPANDAS = geopandas is not None
 
 PANDAS_GE_15 = pandas is not None and Version(pandas.__version__) >= Version("1.5.0")
 PANDAS_GE_20 = pandas is not None and Version(pandas.__version__) >= Version("2.0.0")
-PANDAS_GE_20 = pandas is not None and Version(pandas.__version__) >= Version("2.2.0")
+PANDAS_GE_22 = pandas is not None and Version(pandas.__version__) >= Version("2.2.0")
 
 HAS_GDAL_GEOS = __gdal_geos_version__ is not None
 
