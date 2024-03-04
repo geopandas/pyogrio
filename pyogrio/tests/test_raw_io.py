b114b931293b4633c3240cd3b2de5a23b04643e7
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-03-04 15:13:02 +0100

    TST: avoid/reduce unimportant warnings in tests (#363)

--- pyogrio/tests/test_raw_io.py
+++ pyogrio/tests/test_raw_io.py
@@ -478,6 +478,10 @@ def test_write_no_fields(tmp_path, naturalearth_lowres):
     meta, _, geometry, field_data = read(naturalearth_lowres)
     field_data = None
     meta["fields"] = None
+    # naturalearth_lowres actually contains MultiPolygons. A shapefile doesn't make the
+    # distinction, so the metadata just reports Polygon. GPKG does, so override here to
+    # avoid GDAL warnings.
+    meta["geometry_type"] = "MultiPolygon"
 
     # Test
     filename = tmp_path / "test.gpkg"
@@ -488,7 +492,7 @@ def test_write_no_fields(tmp_path, naturalearth_lowres):
     meta, _, geometry, fields = read(filename)
 
     assert meta["crs"] == "EPSG:4326"
-    assert meta["geometry_type"] == "Polygon"
+    assert meta["geometry_type"] == "MultiPolygon"
     assert meta["encoding"] == "UTF-8"
     assert meta["fields"].shape == (0,)
     assert len(fields) == 0
