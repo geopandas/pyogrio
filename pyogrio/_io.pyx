e65b687fcd92e119cfdea74b792d847b5e02a274
Nicholas YS Tan <39729063+nicholas-ys-tan@users.noreply.github.com>
2024-06-12 23:11:58 +1000

    formatting
    Co-authored-by: Pieter Roggemans <pieter.roggemans@gmail.com>

--- pyogrio/_io.pyx
+++ pyogrio/_io.pyx
@@ -2134,8 +2134,8 @@ cdef create_ogr_dataset_layer(
         if crs is not None:
             try:
                 ogr_crs = create_crs(crs)
-                 # force geographic CRS to use lon, lat order and ignore axis order specified by CRS, in order
-                 # to correctly write KML and GeoJSON coordinates in correct order
+                # force geographic CRS to use lon, lat order and ignore axis order specified by CRS, in order
+                # to correctly write KML and GeoJSON coordinates in correct order
                 OSRSetAxisMappingStrategy(ogr_crs, OAMS_TRADITIONAL_GIS_ORDER)
 
 
