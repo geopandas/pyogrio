d901691b531ca6a1f95027b8bc34bf243128be1f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-11-21 18:03:57 +0100

    fix check for tz on old pandas

--- pyogrio/tests/test_geopandas_io.py
+++ pyogrio/tests/test_geopandas_io.py
@@ -919,7 +919,9 @@ def test_write_read_datetime_tz_objects(
     )
 
     # With some older versions, the offset is represented slightly differently
-    if result.dates.dtype.name.endswith(", pytz.FixedOffset(-300)]"):
+    if not PANDAS_GE_20 and result.dates.dtype.name.endswith(
+        ", pytz.FixedOffset(-300)]"
+    ):
         result["dates"] = result.dates.astype(exp_df.dates.dtype)
 
     if use_arrow and __gdal_version__ < (3, 10, 0) and ext in (".geojson", ".geojsonl"):
@@ -965,11 +967,12 @@ def test_write_read_datetime_tz_objects(
                 # With GDAL < 3.7, timezone minutes aren't included in the string
                 exp_df["dates"] = exp_df.dates.str.slice(0, -3)
     elif mixed_offsets_as_utc:
-        # the offsets are all -05:00, so the result retains the offset
+        # the offsets are all -05:00, so the result retains the offset and not UTC
         assert isinstance(result.dates.dtype, pd.DatetimeTZDtype)
-        assert str(result.dates.dtype.tz) == "UTC-05:00"
-
-        # exp_df["dates"] = exp_df.dates.dt.tz_convert("UTC")
+        if PANDAS_GE_20:
+            assert str(result.dates.dtype.tz) == "UTC-05:00"
+        else:
+            assert str(result.dates.dtype.tz) in ("UTC-05:00", "pytz.FixedOffset(-300)")
     else:
         assert isinstance(result.dates.dtype, pd.DatetimeTZDtype)
 
